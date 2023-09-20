3c94f8968c3a51f6e560216da379b4dc91322b9a
charlie nguyen <charliekid19@gmail.com>
2023-09-20 14:43:15 -0700

    Commit for saving

--- decklist/Reader.js
+++ decklist/Reader.js
@@ -1,86 +1,158 @@
 const fs = require('fs');
 const readlineSync = require('readline-sync');
 
-const filePath = 'path/to/your/file.txt';
+class  Card {
+    constructor(cardType, cardName, set, collectionNum, qty) {
+        this._cardType = cardType;
+        this._cardName = cardName;
+        this._set = set;
+        this._collectionNum = collectionNum;
+        this._qty = qty;
+    }
 
-const fileContent = fs.readFileSync(filePath, 'utf-8');
-const lines = fileContent.split('\n');
 
+    get cardType(){
+        return this._cardType;
+    }
+
+    set cardType(value) {
+        this._cardType = value;
+    }
+
+    get cardName() {
+        return this._cardName;
+    }
+
+    set cardName(value) {
+        this._cardName = value;
+    }
+
+    get set() {
+        return this._set;
+    }
+
+    set set(value) {
+        this._set = value;
+    }
+
+    get collectionNum() {
+        return this._collectionNum;
+    }
+
+    set collectionNum(value) {
+        this._collectionNum = value;
+    }
+
+    get qty() {
+        return this._qty;
+    }
+
+    set qty(value) {
+        this._qty = value;
+    }
+}
+
+let pokemonQtyOutput = "";
+let pokemonNameOutput = "";
+let pokemonSetOutput = "";
+let pokemonCollectionOutput = "";
+
+let trainerQtyOutput = "";
+let trainerNameOutput = "";
+
+let energyQtyOutput = "";
+let energyNameOutput  = "";
 
 let pokemonList = [];
 let tempTrainerList = new Map;
 let energyList = [];
 let trainerList = [];
 
-let currentCardSection = "Pokemon";
+function processDeckList(filePath) {
+    let currentCardSection = "Pokemon";
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    const lines = fileContent.split('\n');
 
-for (const currentLine of lines) {
-    console.log(`Line: ${currentLine}`);
-    if(currentLine.includes('Trainer: ') ) {
-        currentCardSection = 'Trainer';
-    } else if(currentLine.includes('Energy: ')) {
-        currentCardSection = 'Energy';
-    } else if(currentLine.includes('Total Cards: ')) {
-        currentCardSection = 'END';
-    }
-    const splitArray = currentLine.split(" ");
-    let qty;
-    let cardName = "";
-    let set;
-    let collectionNum;
-    if(splitArray.length > 0) {
-        qty = splitArray[0];
-    }
-    // means that there card name has more spaces
-    if(splitArray.length > 4) {
-        let i = splitArray - 1;
-        // we know that last 2 elements are the collection#
-        // and set so lets deal with that.
-        collectionNum = splitArray[i];
-        set = splitArray[i-1];
-
-        // now we know anthing between 1 and (i - 1) is the pokemon card name
-        for(let j = 1 ; j < (i - 1); j++) {
-            cardName += splitArray[j];
+    for (const currentLine of lines) {
+        // console.log(`Line: ${currentLine}`);
+        if (currentLine.includes('Trainer: ')) {
+            currentCardSection = 'Trainer';
+        } else if (currentLine.includes('Energy: ')) {
+            currentCardSection = 'Energy';
+        } else if (currentLine.includes('Total Cards: ')) {
+            currentCardSection = 'END';
         }
-    }
-    if(currentCardSection.equals('Trainer')) {
-        incrementOrAddKey(cardName, tempTrainerList);
-    } else if(currentCardSection.equals('Pokemon')) {
-        pokemonList.push(new Card(currentCardSection, cardName, set, collectionNum, qty));
-    } else if(currentCardSection.equals('Energy')) {
-        energyList.push(new Card(currentCardSection, cardName, set, collectionNum, qty));
-    }
+        const splitArray = currentLine.split(" ");
+        let qty;
+        let cardName = "";
+        let set;
+        let collectionNum;
+        if (splitArray.length > 0) {
+            qty = splitArray[0];
+            console.log(`qty : ${qty}[ LINE ${currentLine}]`);
+        }
+        // means that there card name has more spaces
+        if (splitArray.length > 4) {
+            let i = splitArray - 1;
+            // we know that last 2 elements are the collection#
+            // and set so lets deal with that.
+            collectionNum = splitArray[i];
+            set = splitArray[i - 1];
 
-}
+            console.log(`collectionNum : ${collectionNum} [LINE ${currentLine}]`);
+            console.log(`set : ${set} [LINE ${currentLine}]`);
+
+
+            // now we know anthing between 1 and (i - 1) is the pokemon card name
+            for (let j = 1; j < (i - 1); j++) {
+                cardName += splitArray[j];
+            }
+            console.log(`cardName : ${cardName} [LINE ${currentLine}]`);
+
+        }
+        if (currentCardSection === 'Trainer') {
+            incrementOrAddKey(cardName, tempTrainerList);
+        } else if (currentCardSection === 'Pokemon') {
+            let aCard = new Card(currentCardSection, cardName, set, collectionNum, qty);
+            pokemonList.push(aCard);
+        } else if (currentCardSection === 'Energy') {
+            energyList.push(new Card(currentCardSection, cardName, set, collectionNum, qty));
+        }
+
+    }
 
 // process trainer cards
-for (const [cardName, qty] of tempTrainerList) {
-    trainerList.push(new Card('Trainer', cardName, "NA", "NA", qty));
-}
+    for (const [cardName, qty] of tempTrainerList) {
+        trainerList.push(new Card('Trainer', cardName, "NA", "NA", qty));
+    }
 
 // pokemon section output
-let pokemonQtyOutput = "";
-let pokemonNameOutput = "";
-let pokemonSetOutput = "";
-let pokemonCollectionOutput = "";
 
-for(const pokemonCard of pokemonList) {
-    pokemonQtyOutput += `${pokemonCard.qty()}\n`;
-    pokemonNameOutput += `${pokemonCard.cardName()}\n`;
-    pokemonSetOutput += `${pokemonCard.set()}\n`;
-    pokemonCollectionOutput += `${pokemonCard.collectionNum()}\n`;
-}
+
+    for (const pokemonCard of pokemonList) {
+        pokemonQtyOutput += `${pokemonCard.qty}\n`;
+        pokemonNameOutput += `${pokemonCard.cardName}\n`;
+        pokemonSetOutput += `${pokemonCard.set}\n`;
+        pokemonCollectionOutput += `${pokemonCard.collectionNum}\n`;
+    }
 
 
 // trainer section output
-let trainerQtyOutput = "";
-let trainerName = "";
+
+    for (const trainerCard of trainerList) {
+        trainerQtyOutput += `${trainerCard.qty}\n`;
+        trainerNameOutput += `${trainerCard.name}\n`
+    }
 
 // energy section output
-let energyQty = "";
-let energyName = "";
 
+    for (const energyCard of energyList) {
+        energyQtyOutput += `${energyCard.qty}\n`
+        energyNameOutput += `${energyCard.name}\n`
+    }
+
+    console.log(pokemonNameOutput);
+}
 
 
 function incrementOrAddKey(key, aMap) {
@@ -92,3 +164,9 @@ function incrementOrAddKey(key, aMap) {
         aMap.set(key, 1);
     }
 }
+
+
+
+module.exports = {processDeckList, pokemonQtyOutput , pokemonNameOutput,
+    pokemonSetOutput, pokemonCollectionOutput, trainerQtyOutput,
+    trainerNameOutput, energyQtyOutput, energyNameOutput}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var reader = require("../decklist/Reader")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  reader.processDeckList("decklist.txt");
+  console.log(reader.pokemonNameOutput)
+
   res.render('index', { title: 'Express' });
 });
 
