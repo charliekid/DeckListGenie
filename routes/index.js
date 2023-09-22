d10a7a24970deb49bdf7c27523440817eb075d01
Charlie Nguyen <charliekid19@gmail.com>
2023-09-21 17:37:15 -0700

    added logger

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ var router = express.Router();
 var reader = require("../decklist/Reader")
 var pdfHandler = require('../decklist/PdfHandler');
 
+const {getLogger} = require("log4js");
+const logger = getLogger("index.js");
+
 /* GET home page. */
 router.get('/', async function (req, res, next) {
   // await pdfHandler.main("Name", "IDDDDD", "20/32/2322", "Junior");
@@ -14,25 +17,23 @@ router.get('/', async function (req, res, next) {
 
 router.post('/submit', async function (req, res, next) {
   //save decklist to txt
-  let fileName = 'submittedList.txt';
-  // console.log(req.body.name);
-  fs.writeFile(fileName, req.body.decklist, async (err) => {
-    if (err) {
-      console.error('Error writing to file:', err);
-    } else {
-      console.log(`Text has been written to ${fileName}`);
-      await pdfHandler.main(req.body.name, req.body.playerId, req.body.dob, req.body.division );
-      res.json({
-        successful : true
-      })
-    }
-  });
+  try {
+    let fileName = 'submittedList.txt';
+    // console.log(req.body.name);
+    fs.writeFile(fileName, req.body.decklist, async (err) => {
+      if (err) {
+        console.error('Error writing to file:', err);
+      } else {
+        console.log(`Text has been written to ${fileName}`);
+        await pdfHandler.main(req.body.name, req.body.playerId, req.body.dob, req.body.division );
+        res.json({
+          successful : true
+        })
+      }
+    });
+  } catch (e) {
 
-
-  // send to reader
-  // await pdfHandler.main();
-
-
+  }
 });
 
 module.exports = router;
